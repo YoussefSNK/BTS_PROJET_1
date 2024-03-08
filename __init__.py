eeb055594818c854f4c1d5a055d33bf9348dd583
YoussefSNK <148006064+YoussefSNK@users.noreply.github.com>
2024-03-08 15:07:14 +0100

    j

--- __init__.py
+++ __init__.py
@@ -1,118 +1,124 @@
-from flask import Flask, render_template, jsonify, request, redirect, url_for, flash
-from flask_mysqldb import MySQL
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo
+from flask import Flask, render_template_string, render_template, jsonify, request, redirect, url_for, session
+from flask import render_template
+from flask import json
+from urllib.request import urlopen
+import sqlite3
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
+app = Flask(__name__)                                                                                                                  
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # Clé secrète pour les sessions
 
-app.config['MYSQL_HOST'] = 'mysql-msprtop.alwaysdata.net'
-app.config['MYSQL_USER'] = 'msprtop_admin'
-app.config['MYSQL_PASSWORD'] = 'ePSI2023!'
-app.config['MYSQL_DB'] = 'msprtop_crm'
+# Fonction pour créer une clé "authentifie" dans la session utilisateur
+def est_authentifie():
+    return session.get('authentifie')
+
+@app.route('/lecture')
+def lecture():
+    if not est_authentifie():
+        # Rediriger vers la page d'authentification si l'utilisateur n'est pas authentifié
+        return redirect(url_for('authentification'))
+
+  # Si l'utilisateur est authentifié
+    return "<h2>Bravo, vous êtes authentifié</h2>"
+
+@app.route('/authentification', methods=['GET', 'POST'])
+def authentification():
+    if request.method == 'POST':
+        # Vérifier les identifiants
+        if request.form['username'] == 'admin' and request.form['password'] == 'password': # password à cacher par la suite
+            session['authentifie'] = True
+            # Rediriger vers la route lecture après une authentification réussie
+            return redirect(url_for('lecture'))
+        else:
+            # Afficher un message d'erreur si les identifiants sont incorrects
+            return render_template('formulaire_authentification.html', error=True)
+
+    return render_template('formulaire_authentification.html', error=False)
 
 
-class AuthentificationForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField("s'authentifier")
+@app.route('/fiche_client/<int:post_id>')
+def Readfiche(post_id):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM clients WHERE id = ?', (post_id,))
+    data = cursor.fetchall()
+    conn.close()
+    
+    # Rendre le template HTML et transmettre les données
+    return render_template('read_data.html', data=data)
 
-
-mysql = MySQL(app)
-
+@app.route('/consultation/')
+def ReadBDD():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM clients;')
+    data = cursor.fetchall()
+    conn.close()
+    return render_template('read_data.html', data=data)
 
 @app.route('/')
-def accueil():  
+def hello_world():
     return render_template('hello.html')
 
+@app.route('/fr/')
+def hello_world_fr():
+    return "<h2>Bonjour tout le monde 2!</h2>"
 
-# @app.route("/authentification", methods=['GET', 'POST'])
-# def authentification():
-#     form = AuthentificationForm()
-#     if form.validate_on_submit():
-#         cursor = mysql.connection.cursor()  
-#         query = ("SELECT * FROM utilisateurs WHERE email = %s AND mot_de_passe = %s")
-#         cursor.execute(query, (form.email.data, form.password.data))
-#         row = cursor.fetchone()
-#         mysql.connection.commit()
-#         cursor.close()
-#         if row is not None:
-#             flash('Vous vous Ãªtes authentifiÃ© avec succÃ¨s {}!'.format(form.email.data), 'success')
-#             return render_template('accueil.html')
-#         else:
-#             return render_template('authentification.html', form=form)                
-#     return render_template('authentification.html', form=form)
+# Création d'une nouvelle route pour la lecture de la BDD
+@app.route('/lecture/')
+def ReadBDD2():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM livres;')
+    data = cursor.fetchall()
+    conn.close()
 
+   # Construisez dynamiquement une chaîne de modèle HTML
+    template_string = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Bibliotheque</title>
+    </head>
+    <body>
+        <h1>Contenu de la base de données</h1>
+        <table border="1">
+            <tr>
+                <th>ID</th>
+                <th>DATE</th>
+                <th>TITRE</th>
+                <th>AUTEUR</th>
+            </tr>
+            {% for row in data %}
+            <tr>
+                <td>{{ row[0] }}</td>
+                <td>{{ row[1] }}</td>
+                <td>{{ row[2] }}</td>
+                <td>{{ row[3] }}</td>
+            </tr>
+            {% endfor %}
+        </table>
+    </body>
+    </html>
+    """
+    return render_template_string(template_string, data=data)
 
-# @app.route('/clients')
-# def clients():  
-#     cursor = mysql.connection.cursor()
-#     cursor.execute('SELECT nom, siret, email, telephone, adresse, ville, code_postal, pays, id FROM clients ')
-#     data = cursor.fetchall()
-#     mysql.connection.commit()
-#     cursor.close()
-#     return render_template('clients.html', data=data)
+@app.route('/paris/')
+def meteo():
+    response = urlopen('https://api.openweathermap.org/data/2.5/forecast/daily?q=Paris,fr&cnt=16&appid=bd5e378503939ddaee76f12ad7a97608')
+    raw_content = response.read()
+    json_content = json.loads(raw_content.decode('utf-8'))
+    results = []
+    for list_element in json_content.get('list', []):
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('temp', {}).get('day') - 273.15 # Conversion de Kelvin en °c 
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
+    return jsonify(results=results)
 
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
 
-# @app.route('/modifier_client', methods=['GET', 'POST'])
-# def modifier_client():    
-#     return render_template('modifier_client.html')
-
-
-# @app.route('/supprimer_client', methods=['POST'])
-# def supprimer_client():    
-#     return render_template('supprimer_client.html')
-  
-
-# @app.route('/ajouter_client', methods=['GET', 'POST'])
-# def ajouter_client():    
-#     if request.method == 'POST':
-#         # RÃ©cupÃ©rer les donnÃ©es du formulaire et effectuer le traitement
-#         nom = request.form['nom']
-#         siret = request.form['siret']
-#         email = request.form['email']
-#         telephone = request.form['telephone']
-#         adresse = request.form['adresse']
-#         ville = request.form['ville']
-#         code_postal = request.form['code_postal']
-#         pays = request.form['pays']
-#         # Traiter les donnÃ©es (par exemple, les afficher dans la console)
-#         cursor = mysql.connection.cursor()
-#         cursor.execute("INSERT INTO clients (nom, siret, email, telephone, adresse, ville, code_postal, pays) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);", (nom, siret, email, telephone, adresse, ville, code_postal, pays))
-#         mysql.connection.commit()
-#         cursor.close()
-#         return render_template('confirmation.html')
-#     else:
-#         # Si la mÃ©thode de la requÃªte est GET, renvoyer simplement le formulaire
-#         return render_template('ajouter_client.html')
-
-  
-  
-# @app.route('/produits')
-# def produits():  
-#     cursor = mysql.connection.cursor()
-#     cursor.execute('SELECT id, code_ean, description, prix, quantite, date_creation FROM produits ')
-#     data = cursor.fetchall()
-#     mysql.connection.commit()
-#     cursor.close()
-#     return render_template('produits.html', data=data)
-
-
-# @app.route('/modifier_produit', methods=['GET', 'POST'])
-# def modifier_produit():    
-#     return render_template('modifier_produit.html')
-
-
-# @app.route('/supprimer_produit', methods=['POST'])
-# def supprimer_produit():    
-#     return render_template('supprimer_produit.html')
-  
-
-# @app.route('/ajouter_produit', methods=['GET', 'POST'])
-# def ajouter_produit():    
-#     return render_template('ajouter_produit.html')
-
-
+                                                                                                                                       
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
