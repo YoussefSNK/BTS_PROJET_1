afc251c8df88f18ceb7b9ac901574ddbbd0310b6
YoussefSNK <148006064+YoussefSNK@users.noreply.github.com>
2024-04-08 10:35:57 +0200

    erreur user_id au lieu d'id

--- __init__.py
+++ __init__.py
@@ -86,7 +86,7 @@ def authentification():
             
             conn = sqlite3.connect('database.db')
             cursor = conn.cursor()
-            cursor.execute('SELECT user_id FROM user WHERE login = ? AND password = ?', (request.form['username'], request.form['password'],))
+            cursor.execute('SELECT id FROM user WHERE login = ? AND password = ?', (request.form['username'], request.form['password'],))
             #cursor.execute('SELECT * FROM perso WHERE id = ?', (post_id,))
             data = cursor.fetchall()
             conn.close()
