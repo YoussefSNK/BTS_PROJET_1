838b0f2f368a634814589e62be3a964597b09917
YoussefSNK <myoussef.fadili@gmail.com>
2025-02-19 23:00:34 +0100

    feat: ajout de la liste des champions et de l'assignement de leurs roles

--- routes/skinteam.py
+++ routes/skinteam.py
@@ -12,14 +12,49 @@ def get_db_connection():
     return conn
 
 
+@skinteam_bp.route('/champions')
+def list_champions():
+    conn = get_db_connection()
+    champions = conn.execute("SELECT * FROM champion").fetchall()
+
+    # Récupérer les rôles associés à chaque champion
+    champion_roles = {}
+    for champion in champions:
+        roles = conn.execute("""
+            SELECT role.id, role.nom, role.image_url
+            FROM role
+            JOIN champion_role ON role.id = champion_role.role_id
+            WHERE champion_role.champion_id = ?
+        """, (champion["id"],)).fetchall()
+        champion_roles[champion["id"]] = roles
+
+    roles = conn.execute("SELECT * FROM role").fetchall()
+    conn.close()
+    return render_template("skinteam/champions.html", champions=champions, champion_roles=champion_roles, roles=roles)
+
+
+@skinteam_bp.route('/champions/<int:champion_id>/update_roles', methods=["POST"])
+def update_champion_roles(champion_id):
+    conn = get_db_connection()
+    
+    selected_roles = request.form.getlist("roles")
+    conn.execute("DELETE FROM champion_role WHERE champion_id = ?", (champion_id,))
+
+    for role_id in selected_roles:
+        conn.execute("INSERT INTO champion_role (champion_id, role_id) VALUES (?, ?)", (champion_id, role_id))
+
+    conn.commit()
+    conn.close()
+
+    return redirect(url_for("skinteam_bp.list_champions"))
+
+
 @skinteam_bp.route('/themes')
 def list_themes():
     conn = get_db_connection()
 
-    # Récupérer tous les thèmes
     themes = conn.execute("SELECT * FROM theme").fetchall()
 
-    # Récupérer les skins associés à chaque thème
     theme_skins = {}
     for theme in themes:
         skins = conn.execute("""
