54ec9ffa6e495d9b67d1ef3bcf678644092bc211
YoussefSNK <myoussef.fadili@gmail.com>
2025-03-02 13:16:00 +0100

    ui: les étoiles scintillent

--- static/script/stars.js
+++ static/script/stars.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById('starfield');
 const ctx = canvas.getContext('2d');
 let stars = [];
 const numStars = 200;
+let time = 0;
 
 function resizeCanvas() {
     canvas.width = window.innerWidth;
@@ -12,17 +13,21 @@ function initStars() {
     resizeCanvas();
     stars = [];
     for (let i = 0; i < numStars; i++) {
+        let speed = Math.random() * 0.5 + 0.2;
         stars.push({
             x: Math.random() * canvas.width,
             y: Math.random() * canvas.height,
             size: Math.random() * 2,
-            speed: Math.random() * 0.5 + 0.2
+            speed: speed,
+            phase: Math.random() * Math.PI * 2,
+            frequency: speed * 1 // paramètre changeable
         });
     }
 }
 
 function animateStars() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    time += 0.02;
 
     for (let star of stars) {
         star.y += star.speed;
@@ -31,7 +36,8 @@ function animateStars() {
             star.x = Math.random() * canvas.width;
         }
 
-        ctx.fillStyle = "white";
+        let opacity = 0.5 + 0.5 * Math.sin(time * star.frequency + star.phase);
+        ctx.fillStyle = `rgba(255, 255, 255, ${opacity})`;
         ctx.beginPath();
         ctx.arc(star.x, star.y, star.size, 0, 2 * Math.PI);
         ctx.fill();
